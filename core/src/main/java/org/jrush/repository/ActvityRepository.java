bda6c54961b60023526ae67d540bd4f4e794736c
Seb <sebastien.tauvel@gmail.com>
2015-11-23 12:29:06 +0100

    adding Activity search by candidate's name

--- core/src/main/java/org/jrush/repository/ActvityRepository.java
+++ core/src/main/java/org/jrush/repository/ActvityRepository.java
@@ -2,7 +2,10 @@ package org.jrush.repository;
 
 import org.jrush.domain.Activity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 /**
  * Created by Seb on 23/11/2015.
@@ -10,5 +13,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface ActvityRepository extends MongoRepository<Activity, String> {
 
+    List<Activity> findByExerciceCandidateFirstName(@Param("name") String name);
 }
 
